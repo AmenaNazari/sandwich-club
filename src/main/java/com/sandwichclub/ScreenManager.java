1f52c299b088db62d068b5d2d6413de26bcc46bd
AmenaNazari <amenanazari95@gmail.com>
2025-05-29 13:04:57 -0400

    add home and main options to screen manager class

--- src/main/java/com/sandwichclub/ScreenManager.java
+++ src/main/java/com/sandwichclub/ScreenManager.java
@@ -1,4 +1,23 @@
 package com.sandwichclub;
 
+import java.util.Scanner;
+
 public class ScreenManager {
+    private Scanner scanner;
+
+    public ScreenManager(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void showHome() {
+        System.out.println(" Welcome to Sandwich Club!");
+        System.out.println("Make your day better with one delicious sandwich!");
+    }
+
+    public void showOrderMenu() {
+        System.out.println("\nChoose an option:");
+        System.out.println("1. I want to order");
+        System.out.println("2. I am done!");
+    }
 }
+
