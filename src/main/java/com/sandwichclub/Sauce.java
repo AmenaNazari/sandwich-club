15c8bc6c4bd2f75dfbbb2540d791b48248ed3e9e
AmenaNazari <amenanazari95@gmail.com>
2025-05-29 09:48:37 -0400

    add override methods to sauce class

--- src/main/java/com/sandwichclub/Sauce.java
+++ src/main/java/com/sandwichclub/Sauce.java
@@ -1,4 +1,18 @@
 package com.sandwichclub;
 
 public class Sauce extends Topping {
+    public Sauce(String name) {
+        super(name); // only the name is needed
+    }
+
+    @Override
+    public double getPrice(int size) {
+        return 0.00; // sauces are always free
+    }
+
+    @Override
+    public String toString() {
+        return getName(); // no extra label needed
+    }
 }
+
